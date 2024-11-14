b05decc568ab8046fadcdea04d99ad887ccf4a96
Ahmad Zaki Firdaus <ahmzakif@gmail.com>
2024-11-14 19:59:47 +0700

    adjust navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,15 +2,43 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Navbar: React.FC<{}> = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [isVisible, setIsVisible] = useState(true);
 
   const toggleMenu = () => setIsMenuOpen((prev) => !prev);
 
+  const handleScroll = () => {
+    if (typeof window !== "undefined") {
+      const currentScrollY = window.scrollY;
+      if (currentScrollY > lastScrollY) {
+        setIsVisible(false);
+        setIsMenuOpen(false);
+      } else {
+        setIsVisible(true);
+      }
+      setLastScrollY(currentScrollY);
+    }
+  };
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.addEventListener("scroll", handleScroll);
+      return () => {
+        window.removeEventListener("scroll", handleScroll);
+      };
+    }
+  }, [lastScrollY]);
+
   return (
-    <div className="w-full h-[65px] bg-['#111'] fixed backdrop-blur-sm z-50 px-4 sm:px-10">
+    <div
+      className={`w-full h-[65px] bg-['#111'] fixed backdrop-blur-sm z-50 px-4 sm:px-10 transition-transform duration-300 ${
+        isVisible ? "translate-y-0" : "-translate-y-full"
+      }`}
+    >
       <div className="w-full h-full flex flex-row items-center justify-between m-auto">
         <a title="ahmzakif logo" href="/" className="h-30 w-30 flex flex-row items-center">
           <Image
@@ -24,10 +52,7 @@ const Navbar: React.FC<{}> = () => {
         </a>
 
         {/* Desktop Navigation */}
-        <ul
-          className="hidden md:flex flex-row items-center space-x-4 md:space-x-4"
-          id="navbar-default"
-        >
+        <ul className="hidden md:flex flex-row items-center space-x-4 md:space-x-4" id="navbar-default">
           <li>
             <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#about">
               <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">ABOUT</div>
@@ -54,7 +79,7 @@ const Navbar: React.FC<{}> = () => {
         <div className="hidden md:flex flex-row gap-5">
           <div
             onClick={() => window.open("mailto:ahmzakif@gmail.com")}
-            className="z-[1] bg-transparent padding-10 cursor-pointer bg-black hover:bg-[#2E2E2E] rounded-xl text-white py-2 px-5"
+            className="z-[1] bg-[#2E2E2E] padding-10 cursor-pointer hover:bg-white hover:text-black rounded-xl text-white py-2 px-5"
           >
             Contact
           </div>
@@ -87,37 +112,43 @@ const Navbar: React.FC<{}> = () => {
         <ul
           className="md:hidden absolute top-16 right-0 bg-transparent z-50 w-[180px] py-5 px-4 text-right"
         >
-          <li>
+          
+          <li className="menu-item">
             <Link className="block px-4 py-2 no-underline outline-none hover:no-underline text-white" href="/#about">
               <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">ABOUT</div>
             </Link>
           </li>
-          <li>
+          <li className="menu-item">
             <Link className="block px-4 py-2 no-underline outline-none hover:no-underline text-white" href="/#skills">
               <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">SKILLS</div>
             </Link>
           </li>
-          <li>
+          <li className="menu-item">
             <Link className="block px-4 py-2 no-underline outline-none hover:no-underline text-white" href="/#experience">
               <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">EXPERIENCE</div>
             </Link>
           </li>
-          <li>
+          <li className="menu-item">
             <Link className="block px-4 py-2 no-underline outline-none hover:no-underline text-white" href="/#projects">
               <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">PROJECTS</div>
             </Link>
           </li>
+
           {/* Contact Button Inside Hamburger Menu for Mobile */}
-          <li>
-            <div
-              onClick={() => window.open("mailto:ahmzakif@gmail.com")}
-              className="z-[1] bg-transparent padding-10 cursor-pointer bg-black hover:bg-[#2E2E2E] rounded-xl text-white py-2 px-5"
+          <li className="menu-item">
+            <Link
+              href="mailto:ahmzakif@gmail.com"
+              className="block px-4 py-2 no-underline outline-none hover:no-underline text-white"
             >
-              Contact
-            </div>
+              <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">
+                CONTACT
+              </div>
+            </Link>
           </li>
         </ul>
       )}
+
+      
     </div>
   );
 };
