3b215c4faf6a01367ae88d843040477bd4566cea
Ahmad Zaki Firdaus <ahmzakif@gmail.com>
2024-11-18 01:39:31 +0700

    update navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -134,20 +134,23 @@ const Navbar: React.FC = () => {
           className="md:hidden flex items-center justify-center p-2 text-white"
           onClick={toggleMenu}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4 6h16M4 12h16M4 18h16"
-            />
-          </svg>
+          <div className="relative w-6 h-6">
+            <span
+              className={`absolute top-0 left-0 w-full h-[3px] block bg-white transition-all duration-300 ease-in-out ${
+                isMenuOpen ? "rotate-45 translate-y-[10.5px]" : ""
+              }`}
+            ></span>
+            <span
+              className={`absolute top-[10px] left-0 w-full h-[3px]  block bg-white transition-opacity duration-300 ease-in-out ${
+                isMenuOpen ? "scale-0" : "opacity-100"
+              }`}
+            ></span>
+            <span
+              className={`absolute bottom-0 left-0 w-full h-[3px]  block bg-white transition-all duration-300 ease-in-out ${
+                isMenuOpen ? "-rotate-45 -translate-y-[10.5px]" : ""
+              }`}
+            ></span>
+          </div>
         </button>
       </div>
 
